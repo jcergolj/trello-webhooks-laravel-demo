1923b19f31cdda280eef263175a37dd338e6fcdb
Janez Cergolj <me@jcergolj.me.uk>
2022-10-24 13:15:11 +0200

    bug-fix; get request must return 200 status code

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
 // trello submits get request to webhooks/trello endpoint to verify
 // that endpoint is valid and active
 Route::get('webhooks/trello', function () {
-    return response()->noContent();
+    return response()->json([], Response::HTTP_OK);
 });
